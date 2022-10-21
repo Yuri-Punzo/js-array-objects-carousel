62d38e203b5e9021c94f6e36a3b9fcaf7fcf66b8
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-21 14:35:20 +0200

    working with new objects array

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -21,44 +21,32 @@ BONUS 2  (opzionale):
 Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 
 BONUS 3  (opzionale):
-Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay. */
 
-STRUTTURA DATI:
-Ecco la struttura dati da usare (le immagini sono le stesse della volta scorsa)
 
-const images = [
+const slidesList = [
     {
-        image: 'img/01.webp',
+        image: 'img/01 Spiderman - Miles Morales.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: 'img/02 Ratchet & Clank - Rift Apart.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: 'img/03 Fortnite.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: 'img/04 Stray.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: 'img/05 Avengers.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
-*/
-
-/* creo array con i path delle immagini */
-const slidesList = [
-    "./assets/img/01 Spiderman - Miles Morales.webp",
-    "./assets/img/02 Ratchet & Clank - Rift Apart.webp",
-    "./assets/img/03 Fortnite.webp",
-    "./assets/img/04 Stray.webp",
-    "./assets/img/05 Avengers.webp"
-]
 
 /* seleziono dove mettere le immagini */
 const slidesElement = document.querySelector(".carousel");
@@ -67,16 +55,13 @@ const slidesElement = document.querySelector(".carousel");
    (inoltre se vado a cambiare "= 0" in "= X" posso far partire le slide da un img diversa)*/
 let visibleImg = 0;
 
-/*  -creo un ciclo for
-    -al suo interno creo una var slideslocation che prenda l'url della img dall'array
-    -grazie alla var slidesHtml vado ad assegnare la classe visible solo alla prima immagine (i === 0) usando il metodo if ristretto visto stamani con Fabio
-    -con .insertAdjacentHTML di fatto inserisco l'immagine nell'html */
 for (let i = 0; i < slidesList.length; i++) {
-    const slideLocation = slidesList[i]
-    console.log(slideLocation);
-    const slidesHtml = `<img class="${i === visibleImg ? 'visible' : ''}" src="${slideLocation}" alt="">`
+    const slideListObjects = slidesList[i];
+    //console.log(slideListObjects);
+    //console.log(slideListObjects.image);
+    const slidesHtml = `<img class="${i === visibleImg ? 'visible' : ''}" src="./assets/${slideListObjects.image}" alt="">`
     slidesElement.insertAdjacentHTML("beforeend", slidesHtml)
-    console.log(slidesHtml);
+    //console.log(slidesHtml);
 }
 
 /* creo due variabile che selezionino i pulsanti per farli poi funzionare */
@@ -95,17 +80,16 @@ const nextButton = document.getElementById("next");
     assegno alla img a seguire la classe visible per farla apparire */
 function nextFunction() {
     const allImg = document.querySelectorAll('.carousel > img');
-    console.log("all img", allImg);
+    //console.log("all img", allImg);
     const currentSlide = allImg[visibleImg];
-    console.log(currentSlide);
+    //console.log(currentSlide);
     currentSlide.classList.remove("visible");
     visibleImg++;
-    /* il problema si creava qua quando dopo l'incremento i numeri di visibleImg andavano fuori dal range dell'array (0-4) con questo if, controllo il numero e lo modifico per farlo ciclare  0 1 2 3 4 0 1 2 3 4 0 1 etcc.  */
     if (visibleImg === slidesList.length) {
         visibleImg = 0;
     }
     const nextImg = allImg[visibleImg];
-    console.log(nextImg);
+    //console.log(nextImg);
     nextImg.classList.add("visible")
 }
 
@@ -113,14 +97,13 @@ function nextFunction() {
 function prevFunction() {
     const allImg = document.querySelectorAll('.carousel > img');
     const currentSlide = allImg[visibleImg];
-    console.log(currentSlide);
+    //console.log(currentSlide);
     currentSlide.classList.remove("visible");
     visibleImg--;
-    /* stessa cosa di sopra ma al rovescio, se il valore scende sotto lo zero lo riportiamo al numero massimale dell'array, ovvero 4*/
     if (visibleImg === -1) {
         visibleImg = 4;
     }
     const prevImg = allImg[visibleImg];
-    console.log(prevImg);
+    //console.log(prevImg);
     prevImg.classList.add("visible")
 }
